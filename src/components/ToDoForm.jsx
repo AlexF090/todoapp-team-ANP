3f9ffe885d2de6c6217b5d7ceb5de5d752e71ae0
Philipp Erker <Philipp.erker@protonmail.com>
2022-04-03 15:34:28 +0200

    add folder components and file ToDoForm.jsx

---
+++ src/components/ToDoForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ToDoForm() {
+  return (
+    <div>
+      <form className="ToDoForm">
+        <input type="text"></input>
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoForm;
